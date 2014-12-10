632a88bff6854442b6796027c27969b106249005
Kazuki Suda <kazuki.suda@gmail.com>
2014-12-10 22:56:37 +0900

    deps: use nopt as CLI parser

--- capture.js
+++ capture.js
@@ -3,17 +3,59 @@
 // vim: set fenc=utf-8 ts=4 sts=4 sw=4 :
 'use strict';
 
-var opts      = require('opts'),
-    opts_conf = require('./opts.json'),
-    cjs       = require('./lib/capturejs');
+var url = require('url'),
+    path = require('path'),
+    nopt = require('nopt'),
+    cjs = require('./lib/capturejs');
 
-opts.parse(opts_conf);
+var knownOpts = {
+        uri: url,
+        output: path,
+        'ssl-protocol': ['sslv3', 'sslv2', 'tlsv1', 'any'],
+        'ignore-ssl-errors': [Boolean, 'true', 'false', 'yes', 'no'],
+        selector: String,
+        'user-agent': String,
+        'javascript-file': path,
+        viewportsize: String,
+        'cookies-file': path,
+        timeout: Number,
+        renderdely: Number,
+        zoomfactor: Number
+    },
+    shortHands = {
+        u: '--uri',
+        o: '--output',
+        p: '---ssl-protocol',
+        I: '--ignore-ssl-errors',
+        s: '--selector',
+        A: '--user-agent',
+        J: '--javascript-file',
+        V: '--viewportsize',
+        c: '--cookies-file',
+        T: '--timeout',
+        R: '--renderdely',
+        z: '--zoomfactor'
+    },
+    errors = [],
+    parsed;
 
-var option = {};
-opts_conf.forEach(function (o) {
-    if (opts.get(o.long)) {
-        option[o.long] = opts.get(o.long);
-    }
-});
+nopt.invalidHandler = function (key, val) {
+    errors.push('Invalid: --' + key + '=' + val);
+};
 
-cjs.capture(option);
+parsed = nopt(knownOpts, shortHands, process.argv);
+
+if (!parsed.uri) {
+    errors.push('Missing required: --uri');
+}
+
+if (!parsed.output) {
+    errors.push('Missing required: --output');
+}
+
+if (errors.length > 0) {
+    console.error(errors.join('\n'));
+    process.exit(1);
+}
+
+cjs.capture(parsed);
